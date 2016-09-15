a7b925eac6542f90fcd4e0ae0cda92bec85e24a7
Simbory <simbory@sina.cn>
2016-09-15 10:28:54 +0800

    fix route bug

--- app.go
+++ app.go
@@ -8,24 +8,87 @@ import (
 )
 
 // CtxHandler the error handler define
-type CtxHandler func(*http.Request) *Result
+type CtxHandler func(*http.Request) Result
 
 // FilterFunc request filter func
 type FilterFunc func(ctx Context)
 
-// NewServer create a new server based on the server root dir
-func NewServer(webRoot string) Server {
-	return newServer(webRoot)
+// RootDir get the root file path of the web server
+func RootDir() string {
+	return defaultServer.RootDir()
 }
 
-// WaitForExit in if there is two or more server running in single process, the WaitForExit function should be called to prevent the main function return immediately
-//noinspection GoUnusedExportedFunction
-func WaitForExit() {
-	serverWaiting.Wait()
+// Config get the config data
+func Config() Configuration {
+	return defaultServer.Config()
+}
+
+// MapPath Returns the physical file path that corresponds to the specified virtual path.
+// @param virtualPath: the virtual path starts with
+// @return the absolute file path
+func MapPath(virtualPath string) string {
+	return defaultServer.MapPath(virtualPath)
+}
+
+// Namespace return the namespace by name
+func Namespace(ns string) Namespace {
+	return defaultServer.Namespace(ns)
+}
+
+// AddViewFunc add the view func map
+func AddViewFunc(name string, f interface{}) {
+	defaultServer.AddViewFunc(name, f)
+}
+
+// AddRouteFunc add the route analyze helper function to server
+func AddRouteFunc(name string, f RouteFunc) {
+	defaultServer.AddRouteFunc(name, f)
+}
+
+// SetRootDir set the webroot of the web application
+func SetRootDir(rootDir string) {
+	defaultServer.SetRootDir(rootDir)
+}
+
+// SetViewExt set the view file extension
+func SetViewExt(ext string) {
+	defaultServer.SetViewExt(ext)
+}
+
+// StaticDir set the path as a static path that the file under this path is served as static file
+// @param pathPrefix: the path prefix starts with '/'
+func StaticDir(pathPrefix string) {
+	defaultServer.StaticDir(pathPrefix)
+}
+
+// StaticFile serve the path as static file
+func StaticFile(path string) {
+	defaultServer.StaticFile(path)
+}
+
+// HandleError handle the error code with the error handler
+func HandleError(errorCode int, handler CtxHandler) {
+	defaultServer.HandleError(errorCode, handler)
+}
+
+// Route set the route rule
+func Route(routePath string, c interface{}, defaultAction ...string) {
+	defaultServer.Route(routePath, c, defaultAction...)
+}
+
+// Filter set the route filter
+func Filter(pathPrefix string, filter FilterFunc) {
+	defaultServer.Filter(pathPrefix, filter)
+}
+
+// Run run the web application
+func Run(port int) {
+	defaultServer.Run(port)
 }
 
 // App the application singleton
 var (
+	defaultServer = newServer(WorkingDir())
 	serverWaiting = sync.WaitGroup{}
 )
 
