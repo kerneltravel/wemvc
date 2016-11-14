b1c7b8985be49a772dc0948ab3e0a7f9cb2028f3
Simbory <simbory@sina.cn>
2016-11-14 23:09:45 +0800

    fix cache bugs

--- cache.go
+++ cache.go
@@ -176,25 +176,3 @@ func newCacheManager(fw *FileWatcher) *CacheManager {
 		fileWatcher: fw,
 	}
 }
-
-type cacheDetector struct {
-	cacheManager *CacheManager
-}
-
-func (cd *cacheDetector) CanHandle(path string) (bool, interface{}) {
-	for name, data := range cd.cacheManager.dataMap {
-		if len(data.dependencies) == 0 {
-			continue
-		}
-		for _, file := range data.dependencies {
-			if strings.EqualFold(file, path) {
-				return true, name
-			}
-		}
-	}
-	return false, nil
-}
-
-func newCacheDetector(manager *CacheManager) *cacheDetector {
-	return &cacheDetector{cacheManager: manager}
-}
